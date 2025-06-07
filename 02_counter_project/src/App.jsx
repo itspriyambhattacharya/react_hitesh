3e27800130db6f7b7361cc4f3bcafd377692ff40
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-07 20:09:44 +0530

    refactor App component to remove direct DOM manipulation and use event handlers for button clicks

--- 02_counter_project/src/App.jsx
+++ 02_counter_project/src/App.jsx
@@ -3,37 +3,32 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-// function addValue(a) {
-//   a++;
-//   return a;
-// }
+function addValue(a) {
+  console.log("Adding Value", Math.random());
 
-// function decValue(a) {
-//   a--;
-//   return a;
-// }
+  // a++;
+  // return a;
+}
+
+function decValue(a) {
+  console.log("Decreasing Value", Math.random());
+  // a--;
+  // return a;
+}
 
 function App() {
   let counter = 3;
-  const addBtn = document.getElementById("addVal");
-  console.log(addBtn);
-
-  addBtn.addEventListener("click", () => {
-    console.log("Adding Value");
-  });
-
-  const delBtn = document.querySelector("#remVal");
-  console.log(delBtn);
-  delBtn.addEventListener("click", () => {
-    console.log("Deleting Value");
-  });
 
   return (
     <>
       <h1>Counter Project and learning Hooks</h1>
       <h3>Counter Value: {counter}</h3>
-      <button id="addVal">Add Value</button>
-      <button id="remVal">Remove Value</button>
+      <button id="addVal" onClick={addValue}>
+        Add Value
+      </button>
+      <button id="remVal" onClick={decValue}>
+        Remove Value
+      </button>
     </>
   );
 }
