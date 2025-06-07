a22268880002bf7f92739ece2e71ff193d3eb60d
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-07 12:47:03 +0530

    refactor App component to use fragments for multiple elements

--- 00_vite_react_installation/src/App.jsx
+++ 00_vite_react_installation/src/App.jsx
@@ -1,7 +1,13 @@
 import Priyam from "./Priyam";
 
 function App() {
-  return <Priyam />;
+  return (
+    // fragment -> jsx only returns one element. But that one element can have multiple elements wrapped inside it.
+    <>
+      <Priyam />
+      <h1>Hello From Fragments</h1>
+    </>
+  );
 }
 
 export default App;
