b0d39fd91d2f9deb077a8fad5f206d1aa97e4534
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-07 12:58:45 +0530

    update Priyam component to return multiple elements and enhance comments

--- 00_vite_react_installation/src/Priyam.jsx
+++ 00_vite_react_installation/src/Priyam.jsx
@@ -1,6 +1,11 @@
 function Priyam() {
-  return <h3>Hello Priyam</h3>; // creating my custom function
+  return (
+    <>
+      <h3>Hello Priyam</h3>
+      <h4>Creating a demo component</h4>
+    </>
+  ); // creating my custom function
 }
 
-// exporting my custom function
+// exporting my custom function (component)
 export default Priyam;
