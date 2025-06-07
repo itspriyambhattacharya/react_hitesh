f57e46e4223bcb56abe6a35b60774bdd60492aef
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-07 20:07:50 +0530

    refactor App component to add event listeners for counter buttons and update button IDs (wrong way)

--- 02_counter_project/src/App.jsx
+++ 02_counter_project/src/App.jsx
@@ -3,14 +3,37 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+// function addValue(a) {
+//   a++;
+//   return a;
+// }
+
+// function decValue(a) {
+//   a--;
+//   return a;
+// }
+
 function App() {
   let counter = 3;
+  const addBtn = document.getElementById("addVal");
+  console.log(addBtn);
+
+  addBtn.addEventListener("click", () => {
+    console.log("Adding Value");
+  });
+
+  const delBtn = document.querySelector("#remVal");
+  console.log(delBtn);
+  delBtn.addEventListener("click", () => {
+    console.log("Deleting Value");
+  });
+
   return (
     <>
       <h1>Counter Project and learning Hooks</h1>
       <h3>Counter Value: {counter}</h3>
-      <button>Add Value</button>
-      <button>Remove Value</button>
+      <button id="addVal">Add Value</button>
+      <button id="remVal">Remove Value</button>
     </>
   );
 }
